ae826399cf8d22df80d891bc35592495700445b0
lukasjoc <jochamlu@gmail.com>
2022-01-07 20:31:21 +0100

    order by freq

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-use std::collections::BTreeMap;
+use std::collections::*;
+use std::cmp::Ordering::*;
 
-// TODO: Frequence the string
 // TODO: Coabine 2 least frequent values together until created a binary tree
 // TODO: Traverse the tree backwards up place 0, down place 1 to get the codebook
 
+// TODO: move to struct ?
+//struct HuffNode {
+//    letter: char,
+//    freq: f32,
+//    bit: i8,
+//}
+
 
 fn main() {
-    const A: &str = "A tuple is a collection of values of different types.
-    Tuples are constructed using parentheses (), and each tuple itself is a
-    value with type signature (T1, T2, ...), where T1, T2 are the types of its
-    members. Functions can use tuples to return multiple values, as tuples can
-    hold any number of values.";
+    const A: &str = "Tree";
 
     // letter:(count,freq)
-    let mut tree: BTreeMap<char, (i32, f32)> = BTreeMap::new();
+    let mut tree = BTreeMap::new();
 
     let freq = |len: usize, amount: i32 | -> f32 {
         1f32 / ((len as f32) / amount as f32)
@@ -21,16 +24,20 @@ fn main() {
 
     let chars: Vec<char> = A.chars().collect();
     for c in chars {
-
+        // calculate the entry
         tree.entry(c)
             .and_modify(|e| {
-                let next_count = e.0 + 1;
-                e.0 = next_count;
-                e.1 = freq(A.chars().count(), next_count);
+                let (ref mut count, ref mut fr) = e;
+                *count += 1;
+                *fr=freq(A.chars().count(), *count)
             })
             .or_insert((1, freq(A.chars().count(), 1i32)));
     }
 
-    println!("{:#?}", tree);
+    let mut tree0: Vec<(&char, &mut (i32, f32))> = tree.iter_mut().collect();
+
+    tree0.sort_by(|a, b| b.1.1.partial_cmp(&a.1.1).unwrap_or(Equal));
+
+    println!("{:#?}", tree0);
 }
 
