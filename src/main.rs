a2ade6493e42f5de4b0ea04c32b5ccd96ae2744d
lukasjoc <jochamlu@gmail.com>
2022-01-07 19:04:56 +0100

    add freq map

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
+use std::collections::BTreeMap;
+
+// TODO: Frequence the string
+// TODO: Coabine 2 least frequent values together until created a binary tree
+// TODO: Traverse the tree backwards up place 0, down place 1 to get the codebook
+
 
 fn main() {
-    println!("Hello, this is Huffman 1952");
+    const a: &str = "A tuple is a collection of values of different types. Tuples are constructed using parentheses (), and each tuple itself is a value with type signature (T1, T2, ...), where T1, T2 are the types of its members. Functions can use tuples to return multiple values, as tuples can hold any number of values.";
+
+    // letter:(count,freq)
+    let mut tree: BTreeMap<char, (i32, f32)> = BTreeMap::new();
+
+    let freq = |len: usize, amount: i32 | -> f32 {
+        1f32 / ((len as f32) / amount as f32)
+    };
+
+    let chars: Vec<char> = a.chars().collect();
+    for c in chars {
+
+        tree.entry(c)
+            .and_modify(|e| {
+                let next_count = e.0 + 1;
+                e.0 = next_count;
+                e.1 = freq(a.chars().count(), next_count);
+            })
+            .or_insert((1, freq(a.chars().count(), 1i32)));
+    }
+
+    println!("{:#?}", tree);
 }
+
