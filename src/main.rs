37892bd823089cfca9efbceed64d26ea44e5a7f5
lukasjoc <jochamlu@gmail.com>
2022-01-09 14:20:18 +0100

    build the tree

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,56 @@
 use std::collections::*;
 
-// TODO:
-// take sorted list of nodes by freq for each item
-
-// take last 2 nodes and combine to new nodes with left and right Branch
-
-// update sorted list and remove used nodes
-
-// do again until freq of last 2 nodes match to 1
-
-// then done and tree should be build and contained in the last node
-
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct HuffNode {
     count: i32,
-    letter: char,
-    // left: ...
-    // right: ...
+    letter: Option<char>,
+    left:  Option<Box<HuffNode>>,
+    right: Option<Box<HuffNode>>,
 }
 
 impl HuffNode {
-    fn new(letter: char, count: i32) -> Self {
+    fn new(count: i32, letter: Option<char>) -> Self {
         Self {
             count,
             letter,
+            ..Default::default()
         }
     }
-}
-
-#[derive(Debug)]
-struct HuffTree {
-    // this is the final node in generation
-    head: Option<HuffNode>,
-}
-
-#[derive(Default)]
-struct HuffMap {
-    map: BTreeMap<char, HuffNode>,
-}
-
-impl HuffMap {
-    fn new() -> Self{
-        Self {
-           map: BTreeMap::new(),
-        }
-    }
-    fn get_last_2(mut self) -> Self {
+    fn left(mut self, node: HuffNode) -> Self {
+        self.left = Some(Box::new(node));
         self
     }
-    fn sort_by_count(mut self) -> Self {
+    fn right(mut self, node: HuffNode) -> Self {
+        self.right = Some(Box::new(node));
         self
-    }
-}
-
-
-impl HuffTree {
-    fn new() -> Self {
-        Self { head: None }
-    }
-    fn get_tree(mut self, a: &str) -> Option<HuffNode> {
-        let head = None;
-        let mut map = HuffMap::new();
-        
-        let chars: Vec<_> = a.chars().collect();
-        for letter in chars {
-            map.map.entry(letter)
-            .and_modify(|e: &mut HuffNode| e.count+=1 )
-            .or_insert(HuffNode::new(letter, 1));
-        }
-
-        // initial sort by count
-        map.sort_by_count();
-
-        // loop while 2 nodes left
-        while map.map.len() > 2 {
-            
-            // determine the last 2 nodes
-            let last_2 = map.get_last_2();
-
-            // build a new node habving the 2 last nodes as branches
-            // and the count as the accum of the 2 nodes
-            let new_node = HuffNode::new(left: last_2.0,
-                                         right, right: last_2.1,
-                                         count: last_2.count + last_1.count);
-            
-            
-            // remove the last 2 nodes from the map of available Nodes
-            map.remove_last_2();
-
-            // insert the new node
-            map.insert(new_node);
-
-            // sort by count again because of insertion
-            map.sort_by_count();
-        }
-
-        // combine the last 2 nodes to the final huffman node
-        // and save as head node
-        head = self.map_to_tree();
-
-        println!("{:#?}", map.map);
-
-        self.head = head;
-        self.head
     }
 }
 
 fn main() {
-    let a = "Tree";
+    let a = "Huffman";
 
-    let tree = HuffTree::new().get_tree(a);
+    let chars: Vec<_> = a.chars().collect();
+    let mut freq = HashMap::new();
+    for letter in chars {
+        freq.entry(letter)
+        .and_modify(|e: &mut HuffNode| e.count+=1 )
+        .or_insert(HuffNode::new(1, Some(letter)));
+    }
 
-    println!("{:#?}", tree);
+    let mut huff_nodes: Vec<HuffNode> = freq.into_iter().map(|e| e.1).collect();
+
+    while huff_nodes.len() > 1 {
+        huff_nodes.sort_by(|a, b| (&(b.count)).cmp(&(a.count)));
+ 
+        let n1 = huff_nodes.pop().unwrap();
+        let n2 = huff_nodes.pop().unwrap();
+
+        let n_new = HuffNode::new(n1.count+n2.count, None)
+            .left(n1)
+            .right(n2);
+
+        huff_nodes.push(n_new);
+    }
+
+    println!("{:#?}", huff_nodes);
 }
