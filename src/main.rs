df40287183542d75839082d6109652b48bcf7fdb
lukasjoc <jochamlu@gmail.com>
2022-01-09 18:11:00 +0100

    add bits without BFS
    this works by retracing

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ use std::collections::*;
 #[derive(Debug, Default)]
 struct HuffNode {
     count: i32,
+    letter: Option<char>,
     book: HashMap<char, Vec<bool>>,
     left: Option<Box<HuffNode>>,
     right: Option<Box<HuffNode>>,
 }
 
 impl HuffNode {
-    fn new(count: i32, book: Option<HashMap<char, Vec<bool>>>) -> Self {
+    fn new(count: i32, letter: Option<char>,
+            book: Option<HashMap<char, Vec<bool>>>) -> Self {
         Self {
             count,
+            letter,
             book: match book {
                 Some(book) => book,
                 None => HashMap::new()
@@ -24,10 +27,31 @@ impl HuffNode {
         self.left = Some(Box::new(node));
         self
     }
+
     fn right(mut self, node: HuffNode) -> Self {
         self.right = Some(Box::new(node));
         self
     }
+
+}
+
+fn map_book<'a >(letter: Option<char>, dir: bool,
+            mut book: &'a mut HashMap<char, Vec<bool>>) -> &'a HashMap<char, Vec<bool>> {
+
+    book = match letter {
+        Some(letter) => {
+            book.insert(letter, vec![dir]);
+            book
+        },
+        None => {
+            for (_, value) in book.iter_mut() {
+                value.push(dir);
+            }
+            book
+        }
+    };
+
+    book
 }
 
 fn main() {
@@ -37,40 +61,44 @@ fn main() {
     let mut freq = HashMap::new();
 
     for letter in chars {
+        let mut node_book = HashMap::new();
+        node_book.insert(letter, vec![false]);
+        let node = HuffNode::new(1, Some(letter), Some(node_book));
         freq.entry(letter)
             .and_modify(|e: &mut HuffNode| {
                 e.count += 1;
                 e.book.insert(letter, vec![false]);
             })
-            .or_insert(HuffNode::new(1, None));
+            .or_insert(node);
     }
 
     let mut huff_nodes: Vec<HuffNode> = freq.into_iter().map(|e| e.1).collect();
-    // println!("{:#?}", huff_nodes);
 
     while huff_nodes.len() > 1 {
         huff_nodes.sort_by(|a, b| (&(b.count)).cmp(&(a.count)));
-        let n1 = huff_nodes.pop().unwrap();
-        let n2 = huff_nodes.pop().unwrap();
+        let mut n1 = huff_nodes.pop().unwrap();
+        let mut n2 = huff_nodes.pop().unwrap();
 
-        let n1_ref = &n1;
-        let n2_ref = &n2;
+        let n1_ref = &mut n1;
+        let n2_ref = &mut n2;
 
-        let book34: HashMap<char, Vec<bool>> = n1_ref
-            .book
+        let mapped_book_left = map_book(n1_ref.letter, true, &mut n1_ref.book);
+        let mapped_book_right = map_book(n2_ref.letter, false, &mut n2_ref.book);
+
+        let book: HashMap<char, Vec<bool>> = mapped_book_left
             .to_owned()
             .into_iter()
-            .chain(n2_ref.book.to_owned())
+            .chain(mapped_book_right.to_owned())
             .collect();
 
-        println!("Books {:?}", book34);
-
-        let n_new = HuffNode::new(n1.count + n2.count, Some(book34)).left(n1).right(n2);
+        let n_new = HuffNode::new(n1.count + n2.count, None, Some(book))
+            .left(n1)
+            .right(n2);
 
         huff_nodes.push(n_new);
     }
 
     let tree: HuffNode = huff_nodes.pop().unwrap();
 
-    // println!("{:#?}", tree);
+    println!("CodeBook for Tree: {:#?}", tree);
 }
